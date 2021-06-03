be135f556a55b8db3e1ada190c6f75c167b9fd6e
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-06-03 10:43:52 -1000

    creating showMovies function. Dynamically creating movie cards

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 // ending search with single double quotes to concat with search
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=ff230f9ce564a1adc6eadf71767706fe&query="'
 
-const form = document.getElementById('form')
-const search = document.getElementById('search')
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 // Get initial movies
 getMovies(API_URL)
@@ -16,7 +17,36 @@ async function getMovies(url){
   const res = await fetch(url)
   const data = await res.json()
 
-  console.log(data.results)
+  // console.log(data.results)
+  showMovies(data.results)
+}
+
+function showMovies(data){
+
+  main.innerHTML = ''
+
+  data.forEach((movie) => {
+    // destructuring
+    const { title, poster_path, vote_average, overview } = movie
+
+    //constructing div with real movie data and then putting it into DOM
+    const movieElement = document.createElement('div')
+    movieElement.classList.add('movie')
+
+    movieElement.innerHTML = `<div class="movie">
+    <img src="${IMG_PATH + poster_path}" alt="${title}">
+    <div class="movie-info">
+      <h3>Movie Title</h3>
+      <span class="green">9.8</span>
+    </div>
+
+    <div class="overview">
+      <h3>Overview</h3>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis est unde tempore dignissimos hic in, ipsum nihil quasi quisquam iure reiciendis deserunt, impedit incidunt voluptatem nobis. Sint aspernatur rerum esse.
+    </div>
+  </div>`
+
+  })
 }
 
 form.addEventListener('submit', (e) => {
